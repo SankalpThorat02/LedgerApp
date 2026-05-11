ae6e255ef2445274a1323fd1a601e7a7d1076c46
SankalpThorat02 <sankalpt2225@gmail.com>
2026-05-11 12:25:28 +0530

    implement Transaction Model

---
+++ src/main/java/com/sankalp/ledger_api/models/Transaction.java
@@ -0,0 +1,36 @@
+package com.sankalp.ledger_api.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Double amount;
+    private String type;
+    private LocalDateTime createdAt;
+
+    public void Transaction() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
